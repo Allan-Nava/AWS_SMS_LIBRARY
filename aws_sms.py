3890f668f4ce00e8eb4fa4e743b1c38c10992871
Allan Nava <allannava95@gmail.com>
2020-01-17 17:00:50 +0100

    Create aws_sms.py

---
+++ aws_sms.py
@@ -0,0 +1,59 @@
+# coding: utf8
+################################################################################
+#
+#   Author          :   Allan Nava
+#   Modifier        :   Allan Nava
+#   Date            :   04/11/2019
+#
+#   AWS SMS Library
+#
+#
+#################################################################################
+#
+import boto3
+#
+#
+class AWS_SMS(object):
+    #
+    AWS_ACCESS_KEY_ID     = 'aws access key id'
+    AWS_SECRET_ACCESS_KEY = 'aws secret access key'
+    #
+    def __init__(self, client='sns'):
+        """
+            :client:    tipo di servizio
+        """
+        #
+        self.client = client
+        #
+        try:
+            print("AWS_SMS initiliaze -> " + self.client)
+            self.sns = boto3.client(
+                self.client,
+                # Hard coded strings as credentials, not recommended.
+                aws_access_key_id=self.AWS_ACCESS_KEY_ID,
+                aws_secret_access_key=self.AWS_SECRET_ACCESS_KEY
+            )
+        except Exception as e:
+            raise Exception("AWS_SMS WS Endpoint not reachable: {}".format(e))
+        #
+    #
+    #
+    def __str__(self):
+        return str(self.sns) + " " + self.client
+    #
+    # - Metodo per mandare un sms
+    def send_sms(self, message="", subject="subject", phoneNumber=""):
+        #
+        #data                = self._prepare_request()
+        #data["PhoneNumber"] = "+393409477141"
+        #data["Message"]     = "message"
+        result              = self.sns.publish(
+            PhoneNumber=phoneNumber,
+            Message=message,
+            Subject=subject
+        )
+        #result              = self.make_request("publish", data)
+        return result
+        #
+    #
+#
